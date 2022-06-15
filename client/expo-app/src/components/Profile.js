4dc816a98204c93e654d33e5ac3a2b5628bee917
akbar <akbarfadil039@gmail.com>
2022-06-15 12:31:34 +0700

    profile

---
+++ client/expo-app/src/components/Profile.js
@@ -0,0 +1,10 @@
+// TODO: answer here
+import React from "react";
+import "../App.css"
+import "../index.css"
+export default function Profile() {
+  // TODO: answer here
+  <div className="profile" aria-label='profile'>
+    <h1>Profile</h1>
+  </div>
+}
