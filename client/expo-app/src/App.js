6eee7ceb19808c6589461db24cb61203c19be30a
azzmnrwebdev <azzmnrwebdev@gmail.com>
2022-06-16 21:47:38 +0700

    add component navbar

--- client/expo-app/src/App.js
+++ client/expo-app/src/App.js
@@ -1,14 +1,23 @@
+<<<<<<< HEAD
 import './App.css';
+=======
+import "./App.css";
+import Navbar from "./components/Navbar";
+>>>>>>> component_header
 
 import FormRegis from './Pages/Form_Regis';
 
 function App() {
+<<<<<<< HEAD
   return (
     <div className="App">
       {/* test halaman regis */}
       <FormRegis />
     </div>
   );
+=======
+  return <Navbar />;
+>>>>>>> component_header
 }
 
 export default App;
